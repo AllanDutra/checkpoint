88f60765823b6e36ed8e0c89ac9e5c59daf6ba0a
AllanDutra <allandutrafeitosa14@gmail.com>
2023-08-05 13:56:58 -0300

    feat: infrastructure module

---
+++ Checkpoint.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,33 @@
+using Checkpoint.Core.Interfaces.Repositories;
+using Checkpoint.Infrastructure.Persistence;
+using Checkpoint.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Checkpoint.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.AddDbContext<CheckpointDbContext>(
+                p => p.UseSqlServer(configuration.GetConnectionString("CheckpointDb"))
+            );
+
+            services.AddRepositories();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            return services;
+        }
+    }
+}
