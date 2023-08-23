d01e507f1cf5cc840f3bd34cb84f8bb009e3c5fd
AllanDutra <allandutrafeitosa14@gmail.com>
2023-08-23 11:22:31 -0300

    feat: Employee filter enum

---
+++ Checkpoint.Core/Enums/EmployeesFilterEnum.cs
@@ -0,0 +1,8 @@
+namespace Checkpoint.Core.Enums
+{
+    public static class EmployeesFilterEnum
+    {
+        public const string AVAILABLE = "AVAILABLE";
+        public const string UNAVAILABLE = "UNAVAILABLE";
+    }
+}
